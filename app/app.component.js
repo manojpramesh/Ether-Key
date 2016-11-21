affe79bd638ebbe5d2c8a66940666411773dfb25
manojpramesh <manojpramesh@gmail.com>
2016-11-21 13:57:25 +0530

    key generation

--- app/app.component.js
+++ app/app.component.js
@@ -9,19 +9,55 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
+var options = {
+    kdf: "pbkdf2",
+    cipher: "aes-128-ctr",
+    kdfparams: {
+        c: 262144,
+        dklen: 32,
+        prf: "hmac-sha256"
+    }
+};
+var params = { keyBytes: 32, ivBytes: 16 };
+var kdf = "pbkdf2";
 var AppComponent = (function () {
     function AppComponent() {
-        this.title = 'Ethereum Wallet';
+        this.password = '';
+        this.privateKey = '';
+        this.address = '';
+        this.keyFile = '';
+        this.fileName = '';
     }
+    AppComponent.prototype.generateKey = function (password) {
+        var dk = keythereum.create(params);
+        for (var i = 0; i < dk.privateKey.length; i++) {
+            this.privateKey += dk.privateKey[i].toString(16);
+        }
+        this.keyObject = keythereum.dump(this.password, dk.privateKey, dk.salt, dk.iv, options);
+        this.address = '0x' + this.keyObject.address;
+        this.keyFile = "data:text;charset=utf-8," + encodeURIComponent(JSON.stringify(this.keyObject));
+        this.fileName = "UTC--" + (new Date).toISOString() + "--" + this.keyObject.address;
+        ;
+    };
+    ;
+    AppComponent.prototype.downloadKeyFile = function () {
+        var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(this.keyObject));
+        var dlAnchorElem = document.getElementById('downloadAnchorElem');
+        dlAnchorElem.setAttribute("href", dataStr);
+        dlAnchorElem.setAttribute("download", "scene.json");
+        dlAnchorElem.click();
+    };
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "\n        <h1>{{title}}</h1>\n        ",
-            styles: ["\n    "]
+            template: "\n        <span class=\"col-md-3\">Enter a password</span>\n        <div class=\"col-md-6\"><input type=\"password\" [(ngModel)]=\"password\"></div>\n        <button class=\"btn btn-success col-md-3\" (click)=\"generateKey()\">Generate</button>\n        <div class=\"row\">\n            <div><label>Private key : </label> {{privateKey}} </div>\n            <div><label>Address : </label> {{address}} </div>\n        </div>\n        <a href=\"{{keyFile}}\" download=\"{{fileName}}\">Get key file</a>\n        ",
+            styles: [""]
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
     return AppComponent;
 }());
 exports.AppComponent = AppComponent;
+function uinthex(ua) {
+}
 //# sourceMappingURL=app.component.js.map
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,59 @@
 import { Component } from '@angular/core';
 
+declare var keythereum;
+var options = {
+    kdf: "pbkdf2",
+    cipher: "aes-128-ctr",
+    kdfparams: {
+        c: 262144,
+        dklen: 32,
+        prf: "hmac-sha256"
+    }
+};
+var params = { keyBytes: 32, ivBytes: 16 };
+var kdf = "pbkdf2";
+
 @Component({
     selector: 'my-app',
     template: `
-        <h1>{{title}}</h1>
-        `,  
-    styles: [`
-    `]
+        <span class="col-md-3">Enter a password</span>
+        <div class="col-md-6"><input type="password" [(ngModel)]="password"></div>
+        <button class="btn btn-success col-md-3" (click)="generateKey()">Generate</button>
+        <div class="row">
+            <div><label>Private key : </label> {{privateKey}} </div>
+            <div><label>Address : </label> {{address}} </div>
+        </div>
+        <a href="{{keyFile}}" download="{{fileName}}">Get key file</a>
+        `,
+    styles: [``]
 })
 
 export class AppComponent {
-    title = 'Ethereum Wallet';
+    password: string = '';
+    privateKey: string = '';
+    address: string = '';
+    keyObject: any;
+    keyFile: string = '';
+    fileName: string = '';
+    generateKey(password) {
+        var dk = keythereum.create(params);
+        for (var i = 0; i < dk.privateKey.length; i++) {
+            this.privateKey += dk.privateKey[i].toString(16);
+        }
+        this.keyObject = keythereum.dump(this.password, dk.privateKey, dk.salt, dk.iv, options);
+        this.address = '0x' + this.keyObject.address;
+        this.keyFile = "data:text;charset=utf-8," + encodeURIComponent(JSON.stringify(this.keyObject));
+        this.fileName = "UTC--" + (new Date).toISOString() + "--" + this.keyObject.address;;
+    };
+
+    downloadKeyFile() {
+        var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(this.keyObject));
+        var dlAnchorElem = document.getElementById('downloadAnchorElem');
+        dlAnchorElem.setAttribute("href", dataStr);
+        dlAnchorElem.setAttribute("download", "scene.json");
+        dlAnchorElem.click();
+    }
+}
+function uinthex(ua) {
+
 }
